4af5e50790662d2de7c800e0f459228fc1657c5a
Alfaid <alfaid786@gmail.com>
2024-09-06 10:32:16 +0530

    First commit

---
+++ src/main/java/com/section2/springframework/guru/DemoSection2SpringframeworkGuruApplication.java
@@ -0,0 +1,13 @@
+package com.section2.springframework.guru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSection2SpringframeworkGuruApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSection2SpringframeworkGuruApplication.class, args);
+	}
+
+}
